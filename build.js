4086a8b3b4e0db1be4d3500c340a81a36a6c28ad
Divy Srivastava <dj.srivastava23@gmail.com>
2021-07-12 09:17:42 +0530

    init

--- build.js
+++ build.js
@@ -1,11 +1,21 @@
+// We're using Node.js here so that we can build our React into HTML.
+// Ok `require` is like import in JS.
+// so we use estrella module to build our JSX into HTML.
+// build function does that... 
+// cliopts stands for CLI options (command line options)
 const { build, cliopts } = require("estrella");
-const path = require("path");
 
+// Here we call the build function..
 build({
+ // We pass it the entry point for out JSX file.
   entry: "index.jsx",
+  // We tell it our final output file.
   outfile: "build/build.js",
+  // We tell it to bundle.
   bundle: true,
+  // This is optional...minify means remove comments and whitespace from code.
   minify: true,
+  // This is optional too...sourcemaps help us debug our code. You will learn about them later.
   sourcemap: false,
   loader: {
     ".js": "jsx",
--- index.jsx
+++ index.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CssBaseline, GeistProvider, Input, Button } from "@geist-ui/react";
+
+function App() {
+    const [todos, setTodos] = React.useState([
+        {
+            text: "test",
+            isDone: false
+        },
+        {
+            text: "test2",
+            isDone: false
+        },
+        {
+            text: "test3",
+            isDone: false
+        }
+    ]);
+    const addtodo = (text) => {
+        // by default new todo will be false done
+        const newTodos = [ ...todos, { text, isDone: false } ];
+        setTodos(newTodos);
+        setInputTodo("");
+    }; 
+    const marktodo = (index) => {
+        const newTodos = todos.find((todo, i) => i === index);
+        
+        newTodos.isDone = true; 
+    }
+
+    const [inputTodo, setInputTodo] = React.useState("");
+    return (
+        <>
+            <Input placeholder="What's on your mind?" onChange={(e) => setInputTodo(e.target.value)} value={inputTodo}>
+            
+            </Input>
+            <Button onClick={() => addtodo(inputTodo)} >Add Todo</Button>
+            { todos.map(todo => <p>{todo.text}</p>) }
+        </>
+    ); 
+}
 
 ReactDOM.render(
-  <div>
-    <h1>Hello, world!</h1>
-  </div>,
+  <GeistProvider>
+    <CssBaseline />
+    <App />
+  </GeistProvider>,
   document.getElementById("root")
-);
+); 
